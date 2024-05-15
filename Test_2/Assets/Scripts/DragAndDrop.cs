77de78a63a461c75bd68e2e4df8c6999ec751728
Anna <anna.salka24@gmail.com>
2024-05-15 16:22:11 +0200

    apple puzzle created

--- Test_2/Assets/Scripts/DragAndDrop.cs
+++ Test_2/Assets/Scripts/DragAndDrop.cs
@@ -7,6 +7,20 @@ public class DragAndDrop : MonoBehaviour
     private Vector3 mousePosition;
     private Color col;
 
+    private bool isMouseDown = false;
+
+    void Update()
+    {        
+        if (isMouseDown)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            { 
+                //Rotation wird getriggert nur wenn linke UND rechte Maustasten gedrükt sind
+                RotateByDegrees();               
+            }
+        }
+    }
+
     private Vector3 GetMousePos()
     {
         print("calculate now");
@@ -19,6 +33,8 @@ public class DragAndDrop : MonoBehaviour
         col = GetComponent<MeshRenderer>().material.color;
         GetComponent<MeshRenderer>().material.color = Color.yellow;
         GetComponent<Rigidbody>().isKinematic = true;
+
+        isMouseDown = true;
     }
 
     private void OnMouseDrag()
@@ -30,5 +46,16 @@ public class DragAndDrop : MonoBehaviour
     {
         GetComponent<MeshRenderer>().material.color = col;
         GetComponent<Rigidbody>().isKinematic = false;
+        isMouseDown= false;
+
+        
+    }
+
+    //rotiert die Objekte nach 45 Grad
+    private void RotateByDegrees()
+    {   
+        Vector3 rotationToAdd = new Vector3(0, 0, 45);
+        transform.Rotate(rotationToAdd);
+       
     }
 }
