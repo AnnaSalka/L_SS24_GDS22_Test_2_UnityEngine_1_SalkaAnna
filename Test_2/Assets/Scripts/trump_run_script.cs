133a2111d04c8b4e05fb05843ccb2f9a6ec1aa88
Anna <anna.salka24@gmail.com>
2024-05-16 16:10:13 +0200

    ragdoll erstellt

---
+++ Test_2/Assets/Scripts/trump_run_script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trump_run_script : MonoBehaviour
+{
+    private float runDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position = new Vector3 (8f, 0f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
